136cafeb6fed59d0150adac8756164faeb6cb05a
Leif Mørstad <leif@moerstad.no>
2025-06-21 23:25:36 +0200

    added new fluid state

--- lib/src/features/fluid/domain/fluid.dart
+++ lib/src/features/fluid/domain/fluid.dart
@@ -1,6 +1,15 @@
 import 'package:get/get_rx/src/rx_types/rx_types.dart';
 
-enum FluidStatus { loading, success, error }
+enum FluidStatus {
+  // No data is available yet, waiting for the first fetch
+  waiting,
+  // The data is being fetched
+  loading,
+  // The data has been successfully fetched
+  success,
+  // An error occurred while fetching the data
+  error,
+}
 
 class Fluid<T> {
   final T? data;
@@ -8,19 +17,16 @@ class Fluid<T> {
   final StackTrace? stackTrace;
   final FluidStatus status;
 
-  const Fluid._({
-    this.data,
-    this.error,
-    this.stackTrace,
-    required this.status,
-  });
+  const Fluid._({this.data, this.error, this.stackTrace, required this.status});
   bool get isSuccess => status == FluidStatus.success;
   bool get isLoading => status == FluidStatus.loading;
   bool get isError => status == FluidStatus.error;
+  bool get isWaiting => status == FluidStatus.waiting;
 
   static Fluid<T> success<T>(T data) {
     return Fluid<T>._(data: data, status: FluidStatus.success);
   }
+
   static Fluid<T> err<T>(Object error, [StackTrace? stackTrace]) {
     return Fluid<T>._(
       error: error,
@@ -28,14 +34,20 @@ class Fluid<T> {
       status: FluidStatus.error,
     );
   }
+
   static Fluid<T> loading<T>() {
     return Fluid<T>._(status: FluidStatus.loading);
   }
+
+  static Fluid<T> waiting<T>() {
+    return Fluid<T>._(status: FluidStatus.waiting);
+  }
+
   static Rx<Fluid<T>> init<T>([T? initialData]) {
     if (initialData != null) {
       return Fluid.success<T>(initialData).obs;
     }
-    return Fluid.loading<T>().obs;
+    return Fluid.waiting<T>().obs;
   }
 }
 
@@ -43,13 +55,17 @@ extension GetAsyncExtension<T> on Rx<Fluid<T>> {
   bool get isSuccess => value.isSuccess;
   bool get isLoading => value.isLoading;
   bool get isError => value.isError;
+  bool get isWaiting => value.isWaiting;
 
   T? get data => value.data;
   Object? get error => value.error;
   StackTrace? get stackTrace => value.stackTrace;
 
-  Future<void> updateAsync(Future<T> Function() future, [bool keepOld = false]) async {
-    if (!keepOld) value = Fluid.loading();
+  Future<void> updateAsync(
+    Future<T> Function() future, [
+    bool keepOld = false,
+  ]) async {
+    if (!keepOld || value.isWaiting) value = Fluid.loading();
     try {
       value = Fluid.success(await future());
     } catch (e, stack) {
--- lib/src/features/fluid/presentation/loading.dart
+++ lib/src/features/fluid/presentation/loading.dart
@@ -2,13 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:innsikt/src/components/info_card.dart';
 import 'package:innsikt/src/features/fluid/domain/fluid.dart';
+import 'package:logger/logger.dart';
 
 typedef ComponentBuilder<T> = Widget Function(T value);
 typedef LoadingComponentBuilder = Widget Function();
 typedef ErrorComponentBuilder =
     Widget Function(Object error, StackTrace? stackTrace);
-
-class LoadingController extends GetxController {}
 
 Widget _defaultLoadingBuilder() {
   return const Center(child: CircularProgressIndicator());
@@ -18,10 +17,13 @@ Widget _defaultErrorBuilder(Object error, StackTrace? stackTrace) {
   return InfoCard.error(error.toString());
 }
 
-class Loading<T> extends GetView<LoadingController> {
+class Loading<T> extends StatelessWidget {
+  static final logger = Logger();
+
   final Rx<Fluid<T>> value;
   final ComponentBuilder<T> builder;
   final LoadingComponentBuilder loadingBuilder;
+  final LoadingComponentBuilder waitingBuilder;
   final ErrorComponentBuilder errorBuilder;
 
   const Loading({
@@ -30,17 +32,24 @@ class Loading<T> extends GetView<LoadingController> {
     required this.builder,
     this.errorBuilder = _defaultErrorBuilder,
     this.loadingBuilder = _defaultLoadingBuilder,
+    this.waitingBuilder = _defaultLoadingBuilder,
   });
 
   @override
   Widget build(BuildContext context) {
-    Get.put(LoadingController());
-
     return Obx(() {
+      if (value.isError) {
+        logger.e(
+          'Error in Loading widget',
+          error: value.error,
+          stackTrace: value.stackTrace,
+        );
+      }
       return switch (value.value.status) {
         FluidStatus.success => builder(value.value.data!),
         FluidStatus.loading => loadingBuilder(),
-        FluidStatus.error => errorBuilder(value.error!, value.stackTrace)
+        FluidStatus.error => errorBuilder(value.error!, value.stackTrace),
+        FluidStatus.waiting => waitingBuilder(),
       };
     });
   }
